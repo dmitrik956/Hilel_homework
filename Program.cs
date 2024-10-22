ce1d7b6c2c7066f3f152d6cd0c389f8391d14f1a
dmitrik956 <diman200479@gmail.com>
2024-10-22 09:46:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Вкажіть цифру від 1 до 7");
+while (true)
+{
+    switch (byte.Parse(Console.ReadLine()!))
+    {
+        case 1:
+            Console.WriteLine("Понеділок");
+            break;
+        case 2:
+            Console.WriteLine("Вівіторок");
+            break;
+        case 3:
+            Console.WriteLine("Середа");
+            break;
+        case 4:
+            Console.WriteLine("Четвер");
+            break;
+        case 5:
+            Console.WriteLine("П'ятниця");
+            break;
+        case 6:
+            Console.WriteLine("Субота");
+            break;
+        case 7:
+            Console.WriteLine("Неділя");
+            break;
+        default:
+            Console.WriteLine("Некортектна цифра");
+            break;
+    };
+}
+
+
+
