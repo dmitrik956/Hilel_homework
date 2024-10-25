277eea244ce3d5d01441e0d3d876b5cae96ba14e
dmitrik956 <diman200479@gmail.com>
2024-10-25 10:55:49 +0300

    Add files via upload

---
+++ hm5.1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Первое  число");
+int n1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Второе  число");
+int n2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Попробуйте деление");
+char one = char.Parse(Console.ReadLine()!); ;
+int res;
+
+try
+{
+    int n1 / n2;
+
+    Console.WriteLine(res);
+}
+catch
+{
+    Console.WriteLine("Делить на 0 невозможно");
+}
