55b1654399b07ca5b9a45f274e26461e7b0da195
dmitrik956 <diman200479@gmail.com>
2024-10-25 09:36:29 +0300

    Add files via upload

---
+++ hm4.2/Program.cs
@@ -0,0 +1,57 @@
+﻿bool continueProgram = true;
+while (continueProgram)
+{
+    Console.WriteLine("Первое  число");
+    int n1 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Второе  число");
+    int n2 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Математическую операция");
+    char one = char.Parse(Console.ReadLine()!); ;
+    int res;
+    switch (one)
+    {
+        case '-':
+            res = n1 - n2;
+            Console.WriteLine($"Результат: {n1} - {n2} = {res}");
+            break;
+        case '+':
+            res = n1 + n2;
+            Console.WriteLine($"Результат: {n1} + {n2} = {res}");
+            break;
+        case '*':
+            res = n1 * n2;
+            Console.WriteLine($"Результат: {n1} * {n2} = {res}");
+            break;
+        case '/':
+            if (n2 != 0)
+            {
+                res = n1 / n2;
+                Console.WriteLine($"Результат: {n1} / {n2} = {res}");
+            }
+            else
+                Console.WriteLine("На 0 делить нельзя");
+            break;
+        default:
+            Console.WriteLine("Некорректная математическая операция");
+            break;
+    }
+    Console.WriteLine("Хотите продолжить? (да/нет)");
+    string? answer = Console.ReadLine()?.ToLower();
+
+    if (answer != "да")
+    {
+        continueProgram = false;
+        Console.WriteLine("Программа завершена.");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
