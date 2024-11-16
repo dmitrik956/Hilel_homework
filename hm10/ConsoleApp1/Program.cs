7a54103e7c4ca5df56d42c78b533faa62d3c0dcf
dmitrik956 <diman200479@gmail.com>
2024-11-16 18:31:54 +0300

    Add files via upload

---
+++ hm10/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите имя и фамилию: ");
+        string fullName = Console.ReadLine()!;
+
+        string[] parts = fullName.Split(' ');
+
+        if (parts.Length >= 2)
+        {
+            string firstName = parts[0];
+            string lastName = parts[1];
+
+            if (char.ToLower(firstName[0]) == char.ToLower(lastName[0]))
+            {
+                Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я");
+            }
+            else
+            {
+                Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Помилка: Введіть і ім’я, і прізвище.");
+        }
+
+        Console.Write("Введите строку, разделенную запятыми: ");
+        string commaSeparated = Console.ReadLine()!;
+        string result = commaSeparated.Replace(" ", "");
+
+        Console.WriteLine("Результат без пробелов: " + result);
+    }
+}
